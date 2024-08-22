8ca69dee10e5554070fd70270d3810645db4fa25
LDLaura <despesselaura@yahoo.fr>
2024-08-22 14:54:50 +0200

    feat(user): read

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -142,4 +142,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullName(): string
+    {
+        return "$this->first_name $this->last_name";
+    }
 }
